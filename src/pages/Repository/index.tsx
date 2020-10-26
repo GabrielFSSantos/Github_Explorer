72b914e6df443381890bf7c0333f6afcdd9b59c6
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-10-26 17:52:00 -0300

    Estilizando Detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+
 
 interface RepositoryParams {
   repository: string;
@@ -7,9 +12,47 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
 
-  const {params} = useRouteMatch<RepositoryParams>();
+  const { params } = useRouteMatch<RepositoryParams>();
 
-return <h1>Repository:{params.repository}</h1>
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="AAAAA" />
+          <div>
+            <strong>AAAAA</strong>
+            <p>BBBBB</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>AAAAA</strong>
+            <span>BBBBB</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link key={} to={``}>
+          <div>
+            <strong>{}</strong>
+            <p>{}</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+
+    </>
+  );
 }
 
 export default Repository;
